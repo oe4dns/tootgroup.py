c79f2788382d3548f5868620cf1247e40ea3dfbc
Andreas Schreiner <andreas.schreiner@sonnenmulde.at>
2019-04-26 00:02:24 +0200

    Make the PyPI package a script

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="tootgroup.py-oe4dns",
+    name="tootgroup.py",
     version="0.9.0",
     author="Andreas Schreiner",
     author_email="andreas.schreiner@sonnenmulde.at",
@@ -13,9 +13,17 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/oe4dns/tootgroup.py",
     packages=setuptools.find_packages(),
+    keywords="mastodon toot group fediverse",
     classifiers=[
+        "Environment :: Console",
+        "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        "Mastodon.py",
+        "appdirs",
+    ],
+    scripts=["tootgroup.py"],
 )
